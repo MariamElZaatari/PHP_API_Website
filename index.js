1b11f67243f4c8872923b5b038dcb58e503a68a1
MariamElZaatari <mariamalzaatari@gmail.com>
2022-02-09 22:01:49 +0200

    JS Added with Buttons Functions

---
+++ index.js
@@ -0,0 +1,60 @@
+function api1(){
+    var x=document.getElementById("x");
+    var y=document.getElementById("y");
+    var result=document.getElementById("result_api1");
+
+    fetch(`Api/Router.php/api1?x=${x.value}&y=${y.value}`)
+    .then(response => response.json())
+    .then(data => {
+        result.innerText=data.result;
+        x.value="";
+        y.value="";
+     }
+    );
+}
+
+function api2(){
+    var text=document.getElementById("text_api2");
+    var result=document.getElementById("result_api2");
+    
+    fetch(`Api/Router.php/api2?text=${text.value}`)
+    .then(response => response.json())
+    .then(data=>{
+        if(data.Palindrome){
+            result.innerText="Palindrome"
+        }else{
+            result.innerText="Not a Palindrome"
+        }
+        text.value="";
+    })
+}
+
+function api3(){
+    var text=document.getElementById("text_api3");
+    var result=document.getElementById("result_api3");
+    
+    fetch(`Api/Router.php/api3?text=${text.value}`)
+    .then(response => response.json())
+    .then(data=>{
+        result.innerText=data.trimmed;
+        text.value="";
+    })
+}
+
+function api4(){
+    var pass=document.getElementById("pass");
+    var result=document.getElementById("result_api4");
+    
+    fetch(`Api/Router.php/api4?pass=${pass.value}`)
+    .then(response => response.json())
+    .then(data=>{
+        result.innerText=`Password is`;
+        if(data.valid){
+            result.innerText+=` Valid.`;
+        }else{
+            result.innerText+=` Not Valid.`;
+        }
+        result.innerText+=` Password: ${data.password}`
+        pass.value="";
+    })
+}
